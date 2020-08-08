19ff96e4023e8e400b950feda8b3e461f64826d2
Tom Pearce <33258637+Tom-Pearce@users.noreply.github.com>
2020-08-08 16:15:41 +0100

    database connect

--- application/models/authModel.php
+++ application/models/authModel.php
@@ -1,16 +1,11 @@
 <?php
-  $db_host = 'eu-cdbr-west-03.cleardb.net';
-  $db_user = 'bf591c69497412';
-  $db_pass = 'b1a72d1a';
-  $db = 'heroku_f3ea27317562590';
-
   class authModel{
 
     function connect(){
       $mysqli = mysqli_init();
       $mysqli->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, TRUE);
       $mysqli->ssl_set(NULL, NULL, "certs/cleardb-ca.pem", NULL, NULL);
-      $mysqli->real_connect($db_host, $this->db_user, $this->db_pass, $this->db);
+      $mysqli->real_connect('eu-cdbr-west-03.cleardb.net', 'bf591c69497412', 'b1a72d1a', 'heroku_f3ea27317562590');
       return $mysqli;
     }
 
