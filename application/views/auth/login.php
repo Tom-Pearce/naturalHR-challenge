d714277064c9456a7fa05fc855c0096bb5b30150
Tom Pearce <33258637+Tom-Pearce@users.noreply.github.com>
2020-08-08 22:14:32 +0100

    login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,9 +1,22 @@
   <div class="row">
     <div class="col-3">
       <form id="loginForm">
-        <input type="email" class="form-control" name="email"/>
-        <input type="password" class="form-control" name="password"/>
+        <input type="email" class="form-control" name="email" maxlength="128"/>
+        <input type="password" class="form-control" name="password" minlength="8" maxlength="32"/>
         <button type="submit" class="btn btn-success" id="loginBtn">Login</button>
       </form>
     </div>
   </div>
+
+  <script type="text/javascript">
+    $('#loginForm').on('submit', function(e){
+
+      e.preventDefault();
+
+      if(this.checkValidity){
+        alert('win');
+      }else{
+        alert('fail');
+      }
+    });
+  </script>
