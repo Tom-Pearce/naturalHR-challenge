6f8e7442c492938dcdc46aa1c97c57598ec9e372
Tom Pearce <33258637+Tom-Pearce@users.noreply.github.com>
2020-08-08 14:41:30 +0100

    More routing

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
   $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
   $segments = explode('/', $path);
 
-  echo $method = $segments[1];
+  echo $class = (isset($segments[1])) ? ucfirst($segments[1]) : 'Auth';
   $function = (isset($segments[2])) ? $segments[2] : 'index';
+  $params = array_slice($segments, 2);
 
-  if(file_exists('application/' . $method . '.php')){
-    require_once('application/' . $method . '.php');
+  if(file_exists('application/' . $class . '.php')){
+    require_once('application/' . $class . '.php');
 
-    $req = new $method();
-    var_dump($req);
+    $req = new $class();
+    call_user_func_array(array(&$req, $function), $params);
   }else{
     echo 'hello2';
   }
