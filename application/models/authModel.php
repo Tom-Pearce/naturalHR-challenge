f3a882f9381218a7639077cf63665b0e5f8d7d72
Tom Pearce <33258637+Tom-Pearce@users.noreply.github.com>
2020-08-08 15:59:20 +0100

    database connect

--- application/models/authModel.php
+++ application/models/authModel.php
@@ -1,20 +1,21 @@
 <?php
+  $db_host = 'eu-cdbr-west-03.cleardb.net';
+  $db_user = 'bf591c69497412';
+  $db_pass = 'b1a72d1a';
+  $db = 'heroku_f3ea27317562590';
 
   class authModel{
 
-    function __construct(){
-      $db_host = 'eu-cdbr-west-03.cleardb.net';
-      $db_user = 'bf591c69497412';
-      $db_pass = 'b1a72d1a';
-      $db = 'heroku_f3ea27317562590';
-    }
-
-    function user_exists($email, $pass){
-
+    function connect(){
       $mysqli = mysqli_init();
       $mysqli->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, TRUE);
       $mysqli->ssl_set(NULL, NULL, "certs/cleardb-ca.pem", NULL, NULL);
       $mysqli->real_connect($this->db_host, $this->db_user, $this->db_pass, $this->db);
+      return $mysqli;
+    }
+
+    function user_exists($email, $pass){
+      $mysqli = $this->connect();
       echo '<br /><br />';
       var_dump($mysqli);
       echo '<br /><br />';
