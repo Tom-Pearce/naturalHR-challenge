c346ab22570c5701b2c66aa45958d688f44cd8df
Tom Pearce <33258637+Tom-Pearce@users.noreply.github.com>
2020-08-08 13:37:07 +0100

    routing

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
   $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
   $segments = explode('/', $path);
 
+  echo '<pre>';
+  var_dump($_SERVER['REQUEST_URI']);
+  var_dump($path);
+  var_dump($segments);
+  // var_dump($path);
   echo $segments[0];
 
+  echo '</pre>';
  ?>
