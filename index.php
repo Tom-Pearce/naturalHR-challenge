45f2ce23e3e874e5abc656f6aede9f3ec073c511
Tom Pearce <33258637+Tom-Pearce@users.noreply.github.com>
2020-08-08 13:52:36 +0100

    routing

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
   $segments = explode('/', $path);
 
-  $method = $segments[1];
+  echo $method = $segments[1];
   $function = (isset($segments[2])) ? $segments[2] : 'index';
 
   if(file_exists('application/' . $method)){
