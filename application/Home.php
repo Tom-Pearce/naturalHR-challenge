af81507d9c18a54089ef6e01f4c76115a3a366aa
Tom Pearce <33258637+Tom-Pearce@users.noreply.github.com>
2020-08-09 15:40:11 +0100

    file upload

--- application/Home.php
+++ application/Home.php
@@ -1,14 +1,14 @@
 <?php
 require_once 'libraries/authLib.php';
   class Home{
-    
+
     function index(){
 
       $authLib = new authLib();
 
       if($authLib->logged_in()){
         ob_start();
-        include('views/home/file_upload_view');
+        include('views/home/file_upload_view.php');
         ob_end_flush();
       }else{
         http_response_code(401);
