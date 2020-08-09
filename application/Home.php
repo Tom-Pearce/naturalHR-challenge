10125b95ce744b7b7bf02ef197cf19ab1638949d
Tom Pearce <33258637+Tom-Pearce@users.noreply.github.com>
2020-08-09 15:27:31 +0100

    login form

--- application/Home.php
+++ application/Home.php
@@ -1,16 +1,18 @@
 <?php
 require_once 'libraries/authLib.php';
+  class Home{
+    
+    function index(){
 
-  function index(){
+      $authLib = new authLib();
 
-    $authLib = new authLib();
-
-    if($authLib->logged_in()){
-      ob_start();
-      include('views/home/file_upload_view');
-      ob_end_flush();
-    }else{
-      http_response_code(401);
+      if($authLib->logged_in()){
+        ob_start();
+        include('views/home/file_upload_view');
+        ob_end_flush();
+      }else{
+        http_response_code(401);
+      }
     }
   }
 ?>
