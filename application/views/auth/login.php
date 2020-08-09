03e30304a67742613832d876c63dbabe36b55419
Tom Pearce <33258637+Tom-Pearce@users.noreply.github.com>
2020-08-09 16:31:41 +0100

    signup

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,42 +1,50 @@
   <div class="row">
-    <div class="col-6 border-right border-info">
-      <form id="loginForm">
-        <input type="email" class="form-control mb-2" name="email" maxlength="128" placeholder="Email" required/>
-        <input type="password" class="form-control mb-2" name="password" minlength="8" maxlength="32" placeholder="Password" required/>
-        <button type="submit" class="btn btn-success float-right" id="loginBtn">Login</button>
-      </form>
-    </div>
-    <div class="col-6" id="signUpArea">
-      <form id="signupForm">
-        <div class="row mb-2">
-          <div class="col-6">
-            <input type="text" class="form-control" name="first_name" maxlength="64" placeholder="First Name" required/>
-          </div>
-          <div class="col-6">
-            <input type="text" class="form-control" name="last_name" maxlength="64" placeholder="Last Name" required/>
+    <div class="col-12">
+      <div class="card">
+        <div class="card-body">
+
+          <div class="row">
+            <div class="col-6 border-right border-info">
+              <form id="loginForm">
+                <input type="email" class="form-control mb-2" name="email" maxlength="128" placeholder="Email" required/>
+                <input type="password" class="form-control mb-2" name="password" minlength="8" maxlength="32" placeholder="Password" required/>
+                <button type="submit" class="btn btn-success float-right" id="loginBtn">Login</button>
+              </form>
+            </div>
+            <div class="col-6" id="signUpArea">
+              <form id="signupForm">
+                <div class="row mb-2">
+                  <div class="col-6">
+                    <input type="text" class="form-control" name="first_name" maxlength="64" placeholder="First Name" required/>
+                  </div>
+                  <div class="col-6">
+                    <input type="text" class="form-control" name="last_name" maxlength="64" placeholder="Last Name" required/>
+                  </div>
+                </div>
+
+                <div class="row mb-2">
+                  <div class="col-12">
+                    <input type="email" class="form-control" name="email" maxlength="128" placeholder="Email" required/>
+                  </div>
+                </div>
+
+                <div class="row mb-2">
+                  <div class="col-6">
+                    <input type="password" class="form-control" name="password" maxlength="64" placeholder="Password" required/>
+                  </div>
+                  <div class="col-6">
+                    <input type="password" class="form-control" name="password_confirm" maxlength="64" placeholder="Confirm Password" required/>
+                  </div>
+                </div>
+
+                <button type="submit" class="btn btn-success float-right">Signup!</button>
+              </form>
+            </div>
           </div>
         </div>
-
-        <div class="row mb-2">
-          <div class="col-12">
-            <input type="email" class="form-control" name="email" maxlength="128" placeholder="Email" required/>
-          </div>
-        </div>
-
-        <div class="row mb-2">
-          <div class="col-6">
-            <input type="password" class="form-control" name="password" maxlength="64" placeholder="Password" required/>
-          </div>
-          <div class="col-6">
-            <input type="password" class="form-control" name="password_confirm" maxlength="64" placeholder="Confirm Password" required/>
-          </div>
-        </div>
-
-        <button type="submit" class="btn btn-success float-right">Signup!</button>
-      </form>
+      </div>
     </div>
   </div>
-
   <script type="text/javascript">
     $(document).ready(function(){
       $('#loginForm').submit(function(e){
